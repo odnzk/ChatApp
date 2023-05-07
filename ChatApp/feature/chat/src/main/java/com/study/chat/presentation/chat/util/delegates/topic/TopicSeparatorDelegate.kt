2059a0d89677fc480f5624704708b4f0373376b6
Odenezhkina <66617436+Odenezhkina@users.noreply.github.com>
2023-05-07 23:13:50 +0300

    Feature: navigate to topic

--- ChatApp/feature/chat/src/main/java/com/study/chat/presentation/chat/util/delegates/topic/TopicSeparatorDelegate.kt
+++ ChatApp/feature/chat/src/main/java/com/study/chat/presentation/chat/util/delegates/topic/TopicSeparatorDelegate.kt
@@ -4,15 +4,22 @@ import androidx.recyclerview.widget.DiffUtil
 import com.study.chat.presentation.chat.util.model.TopicSeparator
 import com.study.components.recycler.delegates.Delegate
 
-internal class TopicSeparatorDelegate : Delegate<TopicSeparatorViewHolder, TopicSeparator>(
-    isType = { it is TopicSeparator },
-    viewHolderCreator = { TopicSeparatorViewHolder.create(it) },
-    viewBinder = { holder, topic -> holder.bind(topic) },
-    comparator = object : DiffUtil.ItemCallback<TopicSeparator>() {
-        override fun areItemsTheSame(oldItem: TopicSeparator, newItem: TopicSeparator): Boolean =
-            oldItem.topicTitle == newItem.topicTitle && oldItem.calendar == newItem.calendar
+internal class TopicSeparatorDelegate(onTopicClick: ((title: String) -> Unit)?) :
+    Delegate<TopicSeparatorViewHolder, TopicSeparator>(
+        isType = { it is TopicSeparator },
+        viewHolderCreator = { TopicSeparatorViewHolder.create(it, onTopicClick) },
+        viewBinder = { holder, topic -> holder.bind(topic) },
+        comparator = object : DiffUtil.ItemCallback<TopicSeparator>() {
+            override fun areItemsTheSame(
+                oldItem: TopicSeparator,
+                newItem: TopicSeparator
+            ): Boolean =
+                oldItem.topicTitle == newItem.topicTitle && oldItem.calendar == newItem.calendar
 
-        override fun areContentsTheSame(oldItem: TopicSeparator, newItem: TopicSeparator): Boolean =
-            oldItem.topicTitle == newItem.topicTitle && oldItem.calendar == newItem.calendar
-    }
-)
+            override fun areContentsTheSame(
+                oldItem: TopicSeparator,
+                newItem: TopicSeparator
+            ): Boolean =
+                oldItem.topicTitle == newItem.topicTitle && oldItem.calendar == newItem.calendar
+        }
+    )
--- ChatApp/feature/chat/src/main/java/com/study/chat/presentation/util/NavigationExtensions.kt
+++ ChatApp/feature/chat/src/main/java/com/study/chat/presentation/util/NavigationExtensions.kt
@@ -2,10 +2,20 @@ package com.study.chat.presentation.util
 
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.study.chat.presentation.chat.ChatFragment
 import com.study.chat.presentation.chat.ChatFragmentDirections
 
 internal fun Fragment.navigateToEmojiListFragment(resultKey: String) {
     findNavController().navigate(
         ChatFragmentDirections.actionChatFragmentToEmojiListFragment(resultKey)
+    )
+}
+
+internal fun ChatFragment.navigateToChannelTopic(channelTitle: String, topicTitle: String) {
+    findNavController().navigate(
+        ChatFragmentDirections.actionChatFragmentSelf(
+            channelTitle,
+            topicTitle
+        )
     )
 }
